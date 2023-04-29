ae8c1e405b4bef04f2f562103d60c763b2935957
Kevin kodiyan <kevinjolly222@gmail.com>
2023-04-29 10:05:04 +0530

    43

---
+++ src/main/java/com/UST/EmployeeRegistration/repository/EmployeeRepo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.UST.EmployeeRegistration.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.UST.EmployeeRegistration.model.Employee;
+
+
+
+/**
+ * @author Administrator
+ *
+ */
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+
+}
